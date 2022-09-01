c8ba58e4deb028db9c7bf7d1b28be5b142750984
MohamedNajeh <mohamed.najeh.ahmed@gmail.com>
2022-09-01 14:13:05 +0200

    Initial Commit

---
+++ MlagTackr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MlagTackr
+//
+//  Created by Najeh on 01/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
